ba8ed11d2031c734f7c448d6d9168a15dea1a054
Intern Software <vr2023@vortexengg.com>
2023-03-13 16:01:17 +0530

    added

---
+++ builderPatternMethod/Mobile1.java
@@ -0,0 +1,14 @@
+package builderPatternMethod;
+
+public class Mobile1 {
+	
+	public static void main(String[] args) {
+		Mobile mobile = new Mobile.Builder("samsung").setColor("blue").setRam("16GB").build();
+		
+		Mobile mobile1=new Mobile.Builder("oppo").setColor("goldish").setRam("8GB").setProcessor("dual-core").build();
+		System.out.println(mobile);
+		System.out.println(mobile1);
+
+	}
+
+}
